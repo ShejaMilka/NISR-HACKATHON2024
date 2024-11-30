aa1483bca42d22b45f7cbab45b4e11c9d9bccaf6
ShejaMilka <milkagaella2@gmail.com>
2024-11-30 18:49:20 +0200

    Add files via upload

--- server.R
+++ server.R
@@ -7,29 +7,87 @@ library(readxl)
 
 # Define Server
 server <- function(input, output) {
-  # Reactive value to store responses
-  responses <- reactiveValues(
-    name = NULL,
-    color = NULL,
-    shiny_like = NULL,
-    shiny_like = NULL
+ 
+   # Reactive value to store responses
+  questions_phq9 <- reactiveValues(
+    question1 = 0,
+    question2 = 0,
+    question3 = 0,
+    question4 = 0,
+    question5 = 0,
+    question6 = 0,
+    question7 = 0,
+    question8 = 0,
+    question9 = 0
   )
   
-  observeEvent(input$submit, {
-    responses$phq1 <- input$question1
-    responses$phq2 <- input$question2
-    responses$phq3 <- input$question3
-    responses$phq4 <- input$question4
-    responses$phq5 <- input$question5
-    responses$phq6 <- input$question6
-    responses$phq7 <- input$question7
-    responses$phq8 <- input$question8
-    responses$phq9 <- input$question9
-    
-    # Print the responses to the console
-    print(paste("Depression:", responses$phq))
+  observeEvent(input$question1, {
+    questions_phq9$question1 <- as.numeric(input$question1)
   })
   
+  observeEvent(input$question2, {
+    questions_phq9$question2 <- as.numeric(input$question2)
+  })
+  
+  observeEvent(input$question3, {
+    questions_phq9$question3 <- as.numeric(input$question3)
+  })
+  
+  observeEvent(input$question4, {
+    questions_phq9$question4 <- as.numeric(input$question4)
+  })
+  
+  observeEvent(input$question5, {
+    questions_phq9$question5 <- as.numeric(input$question5)
+  })
+  
+  observeEvent(input$question6, {
+    questions_phq9$question6 <- as.numeric(input$question6)
+  })
+  
+  observeEvent(input$question7, {
+    questions_phq9$question7 <- as.numeric(input$question7)
+  })
+  
+  observeEvent(input$question8, {
+    questions_phq9$question8 <- as.numeric(input$question8)
+  })
+  
+  observeEvent(input$question9, {
+    questions_phq9$question9 <- as.numeric(input$question9)
+  })
+  
+  output$phq9_score <- renderText({
+    score_display <-  questions_phq9$question1 + questions_phq9$question2 + 
+      questions_phq9$question3 + questions_phq9$question4 + 
+      questions_phq9$question5 + questions_phq9$question6 + 
+      questions_phq9$question7 + questions_phq9$question8 + 
+      questions_phq9$question9
+    paste("Your PHQ-9 score is", score_display,".")
+  })
+  
+  output$depression_severity <- renderText({
+    score <-  questions_phq9$question1 + questions_phq9$question2 + 
+      questions_phq9$question3 + questions_phq9$question4 + 
+      questions_phq9$question5 + questions_phq9$question6 + 
+      questions_phq9$question7 + questions_phq9$question8 + 
+      questions_phq9$question9
+
+    depression_severity <- if (score >= 0 & score <= 4) {
+      "Minimal depression symptoms"
+    } else if (score >= 5 & score <= 9) {
+      "Mild depression"
+    } else if (score >= 10 & score <= 14) {
+      "Moderate depression"
+    } else if (score >= 15 & score <= 19) {
+      "Moderately severe depression"
+    } else {
+      "Severe depression"
+    }
+    paste("This indicates that you have", depression_severity, ".")
+  })
+  
+
   # Data for mental health status by sex
   mental_health_by_sexrbc <- read_excel("mentalhealth_bysex_rbc.xlsx")
   mental_health_by_sex <- data.frame(mental_health_by_sexrbc)
@@ -99,7 +157,7 @@ server <- function(input, output) {
   # Generate trend plot over years
   output$trend_line <- renderPlot({ 
     ggplot(data_long_rbc, aes(x = Year, y = Prevalence, color = Sex, group = Sex)) +
-      geom_line(size = 1) +
+      geom_line(linewidth = 2) +
       geom_point(size = 3) +
       facet_wrap(~ Disorder, scales = "free_y") +  # Create separate plots for each disorder
       labs(title = "Trend of Mental Disorders Over Years by Sex",
--- ui.R
+++ ui.R
@@ -7,7 +7,7 @@ library(readxl)
 
 # Define UI
 ui <- dashboardPage(skin = "green",
-  dashboardHeader(title = "Insight in Youth Mental Health Issues",
+  dashboardHeader(title = "Youth Mental Health Problems in Rwanda",
                   titleWidth = 350
                   ),
     dashboardSidebar(
@@ -29,22 +29,12 @@ ui <- dashboardPage(skin = "green",
   
     dashboardBody(
       tabItems(
-        tabItem(tabName = "dashboard",
-                fluidRow(
-                  box(plotOutput("bargraph_status")),
-                  box(plotOutput("bargraph_agegrpd")),
-                  box(plotOutput("trend_line")),
-                  box(title = tags$h3("Non-Frequent Mental Health Problem in Youth"), 
-                      tableOutput("table_mentalhealth")
-                  )
-                )
-        ),
-        
-        tabItem( tabName = "overview",
+        tabItem(tabName = "overview",
+            titlePanel(h2("NISR 2024 HACKATHON COMPETITION", style = "font-size: 36px; text-align: center;font-weight: bold;")),
             fluidRow(
-              box(
+              box(solidHeader = TRUE,background = "light-blue",
                 p(style = "font-size: 20px; text-align: justify; line-height: 2;",
-                   "The insight in Youth Mental Health is a web app designed for the purpose of the NISR Hackathon 2024 Competion. 
+                   "The insight Mental Health problems in youth is a web app designed for the purpose of the NISR Hackathon 2024 Competion. 
                    It helps to gain insight into the problems."
                  ),
                 
@@ -56,7 +46,7 @@ ui <- dashboardPage(skin = "green",
                )
              ),
                
-            box(
+            box(solidHeader = TRUE,background = "light-blue",
                p(style = "font-size: 20px;text-align: justify; line-height: 2;" ,
                  "To address these challenges, a multi-faceted approach is necessary. This includes increasing awareness and reducing stigma, 
                  strengthening mental health services, addressing underlying social and economic factors, leveraging technology, 
@@ -73,6 +63,16 @@ ui <- dashboardPage(skin = "green",
              )
             )
           ),
+        tabItem(tabName = "dashboard",
+                fluidRow(
+                  box(plotOutput("bargraph_status")),
+                  box(plotOutput("bargraph_agegrpd")),
+                  box(plotOutput("trend_line")),
+                  box(title = tags$h3("Non-Frequent Mental Health Problem in Youth"), 
+                      tableOutput("table_mentalhealth")
+                  )
+                )
+        ),
         
         tabItem(tabName = "data",
               dataTableOutput("raw_data_nisr"),
@@ -90,91 +90,101 @@ ui <- dashboardPage(skin = "green",
           ),
         
         tabItem(tabName = "assessments",
-            box("Over the last 2 weeks, how often have you been bothered by any of the following problems?", width = 920),
-            fluidRow(
-              box(
-                title = "1.",
+                titlePanel(h2("PHQ-9 Depression Assessment", style = "font-size: 36px; text-align: center;font-weight: bold;")),
+            box(p("Over the last 2 weeks, how often have you been bothered by any of the following problems?"), width = 920, style = "font-size: 20px;text-align: justify; line-height: 2;"),
+            fluidRow(style = "background-color: lightblue;",
+              box(background = "light-blue",
+                  title = "PHQ9-1.",width = 12,
                 selectInput("question1", "Problem of little interest or pleasure in doing things",
-                            choices = c("Not at all", 
-                                        "Several days", 
-                                        " More than half the days", 
-                                        "Nearly every day")
+                            choices = c("Not at all" = 0, 
+                                        "Several days" = 1, 
+                                        "More than half the days" = 2, 
+                                        "Nearly every day" = 3)
                 )
               ),
-              box(
-                title = "2.",
+              box(background = "light-blue",
+                title = "PHQ-2.",width = 12,
                 selectInput("question2", "Problem of feeling down, depressed, or hopeless",
-                            choices = c("Not at all", 
-                                        "Several days",
-                                        " More than half the days",
-                                        "Nearly every day")
+                            choices = c("Not at all" = 0, 
+                                        "Several days" = 1,
+                                        "More than half the days" = 2,
+                                        "Nearly every day" = 3)
                 )
               ),
-              box(
-                title = "3.",
+              box(background = "light-blue",
+                title = "PHQ-3.",width = 12,
                 selectInput("question3", "Problem of trouble falling or staying asleep, or sleeping too much?",
-                            choices = c("Not at all", 
-                                        "Several days", 
-                                        " More than half the days",
-                                        "Nearly every day")
+                            choices = c("Not at all" = 0, 
+                                        "Several days" = 1, 
+                                        "More than half the days" = 2,
+                                        "Nearly every day" = 3)
                 )
               ),
-              box(
-                title = "4.",
+              box(background = "light-blue",
+                title = "PHQ-4.",width = 12,
                 selectInput("question4", "Feeling tired or having little energy",
-                            choices = c("Not at all", 
-                                        "Several days", 
-                                        "More than half the days", 
-                                        "Nearly every day")
+                            choices = c("Not at all" = 0, 
+                                        "Several days" = 1, 
+                                        "More than half the days" = 2, 
+                                        "Nearly every day" = 3)
                 )
               ),
-              box(
-                title = "5.",
-                selectInput("question6", "Poor appetite or overeating",
-                            choices = c("Not at all", 
-                                        "Several days", 
-                                        "More than half the days", 
-                                        "Nearly every day")
+              box(background = "light-blue",
+                title = "PHQ-5.",width = 12,
+                selectInput("question5", "Poor appetite or overeating",
+                            choices = c("Not at all" = 0, 
+                                        "Several days" = 1, 
+                                        "More than half the days" = 2, 
+                                        "Nearly every day" = 3)
                 )
               ),
-              box(
-                title = "6.",
+              box(background = "light-blue",
+                title = "PHQ-6.", width = 12,
                 selectInput("question6", " Feeling bad about yourself – or that you are a failure or have let yourself or your family down",
-                            choices = c("Not at all",
-                                        "Several days",
-                                        "More than half the days",
-                                        "Nearly every day")
+                            choices = c("Not at all" = 0,
+                                        "Several days" = 1,
+                                        "More than half the days" = 2,
+                                        "Nearly every day" = 3)
                 )
               ),
-              box(
-                title = "7.",
+              box(background = "light-blue",
+                title = "PHQ-7.", width = 12,
                 selectInput("question7", "Trouble concentrating on things, such as reading the newspaper or watching television",
-                            choices = c("Not at all",
-                                        "Several days", 
-                                        "More than half the days",
-                                        "Nearly every day")
+                            choices = c("Not at all" = 0,
+                                        "Several days" = 1, 
+                                        "More than half the days" = 2,
+                                        "Nearly every day" = 3)
                 )
               ),
-              box(
-                title = "8.",
+              box(background = "light-blue",
+                title = "PHQ-8.", width = 12,
                 selectInput("question8", "Moving or speaking so slowly that other people could have noticed? Or the opposite – being so fidgety or restless that you have been moving around a lot more than usual",
-                            choices = c("Not at all", 
-                                        "Several days", 
-                                        "More than half the days",
-                                        "Nearly every day")
+                            choices = c("Not at all" = 0, 
+                                        "Several days" = 1, 
+                                        "More than half the days" = 2,
+                                        "Nearly every day" = 3)
                 )
               ),
-              box(
-                title = "9.",
+              box(background = "light-blue",
+                title = "PHQ-9.",width = 12,
                 selectInput("question9", "Thoughts that you would be better off dead or of hurting yourself in some way",
-                            choices = c("Not at all", 
-                                        "Several days", 
-                                        " More than half the days",
-                                        "Nearly every day")
+                            choices = c("Not at all" = 0, 
+                                        "Several days" = 1, 
+                                        " More than half the days" = 2,
+                                        "Nearly every day" = 3)
                 )
               ),
-              box(
+              box(background = "light-blue",
                 actionButton("submit", "Submit")
+              ),
+              box(background = "light-blue",status = "primary",
+                  solidHeader = TRUE,
+                  collapsible = TRUE,
+                  closable = TRUE,
+                  width = 6,
+                  title = "Results of the self Assessment",
+                  textOutput("phq9_score"),
+                  textOutput("depression_severity")
               )
           )
        )
